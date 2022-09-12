6260160f57fd334d5fdc1a8c7821d7b50e256b33
MobbySchiller <kb.burzynski@gmail.com>
2022-09-12 09:47:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './sass/style.scss';
 import './components/header/header';
 import './components/cart/cart';
+import './components/mobile-slider/mobile-slider';
+import './components/details/details';
+import './components/gallery/gallery';
+import './components/lightbox/lightbox';
